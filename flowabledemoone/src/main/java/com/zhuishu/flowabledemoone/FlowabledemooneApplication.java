de0e1339b00be17e983aa4377db508e8bdc86dc5
AlbertZJ <48433532+AlbertZJ@users.noreply.github.com>
2020-07-27 14:24:40 +0800

    flowableOne
    springboot+flowable开始

---
+++ flowabledemoone/src/main/java/com/zhuishu/flowabledemoone/FlowabledemooneApplication.java
@@ -0,0 +1,13 @@
+package com.zhuishu.flowabledemoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowabledemooneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowabledemooneApplication.class, args);
+    }
+
+}
---
+++ flowabledemoone/src/test/java/com/zhuishu/flowabledemoone/FlowabledemooneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuishu.flowabledemoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowabledemooneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
